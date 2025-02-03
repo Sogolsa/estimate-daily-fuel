b0f63a726cd71eb1b5de2fd3b7a77eaf22d50b5b
Sogol <sogol.saadat@gmail.com>
2025-02-02 20:35:02 -0600

    Updated vite config for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Estimate Daily Calories & Macros",
+        short_name: "Calorie Estimator",
+        theme_color: "#fa4454",
+        background_color: "#000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js, css, html,png, svg, ico}"],
+      },
+    }),
+  ],
+  build: {
+    sourcemap: false, // Disable source maps for a faster build
+    chunkSizeWarningLimit: 500, // Avoid warnings for large chunks
+  },
+});
